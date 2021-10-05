061123f68ec356a3254a423b03ef64aaba923028
wearelegionerseka <38984368+wearelegionerseka@users.noreply.github.com>
2021-10-05 14:12:02 +0200

    Add files via upload

--- settings.py
+++ settings.py
@@ -20,62 +20,65 @@ next_image = "images/next.png"
 movieDB_api_key = "8804be4f30f706e9e0ec40c32d961635"
 
 menu_items = [
-	("[B][COLOR lime]Film[/COLOR][/B]", movie_image),
-	("[B][COLOR lime]Serie TV[/COLOR][/B]", tvshow_image),
-]
+	("[B][COLOR yellow]>>>>>[COLOR lime]Film[COLOR yellow]<<<<<[/COLOR][/B]", movie_image),
+    
+    ("[B][COLOR yellow]>>>>>[COLOR lime]Serie TV[COLOR yellow]<<<<<[/COLOR][/B]", tvshow_image),
+    
+    ]
 
 film_menu_items = [
-	("[B][COLOR lime]Ricerca film[/COLOR][/B]", search_movie_image),
-	("[B][COLOR lime]Ricerca persona film[/COLOR][/B]", movie_search_people_image),
-	("[B][COLOR lime]Film i piu popolari[/COLOR][/B]", movie_most_popular_image),
-	("[B][COLOR lime]Film i piu votati[/COLOR][/B]", movie_most_voted_image),
-	("[B][COLOR lime]Film Altamente valutati[/COLOR][/B]", movie_highly_rated_image),
-	("[B][COLOR lime]Film dell'anno[/COLOR][/B]", movie_years_image),
+	("[B][COLOR lime]Ricerca per titolo film[/COLOR][/B]", search_movie_image),
+	("[B][COLOR lime]Ricerca per attore[/COLOR][/B]", movie_search_people_image),
+	("[B][COLOR lime]Film più popolari[/COLOR][/B]", movie_most_popular_image),
+	("[B][COLOR lime]Film più votati[/COLOR][/B]", movie_most_voted_image),
+	("[B][COLOR lime]Film più valutati[/COLOR][/B]", movie_highly_rated_image),
+	("[B][COLOR lime]Film divisi per anno[/COLOR][/B]", movie_years_image),
 	("[B][COLOR lime]Film per genere[/COLOR][/B]", movie_genres_image)
 ]
 
 tvshow_menu_items = [
 	("[B][COLOR lime]Ricerca serie TV[/COLOR][/B]", search_tvshow_image),
-	("[B][COLOR lime]Ricerca persona serie TV[/COLOR][/B]", tvshow_search_people_image),
-	("[B][COLOR lime]Serie TV famosi[/COLOR][/B]", tvshow_most_popular_image),
-	("[B][COLOR lime]Serie TV le piu votate[/COLOR][/B]", tvshow_most_voted_image),
-	("[B][COLOR lime]Serie TV Altamente valutate[/COLOR][/B]", tvshow_highly_rated_image),
-	("[B][COLOR lime]Serie TV dell'anno[/COLOR][/B]", tvshow_years_image),
+	("[B][COLOR lime]Ricerca per attore[/COLOR][/B]", tvshow_search_people_image),
+	("[B][COLOR lime]Serie TV più popolari[/COLOR][/B]", tvshow_most_popular_image),
+	("[B][COLOR lime]Serie TV piu votate[/COLOR][/B]", tvshow_most_voted_image),
+	("[B][COLOR lime]Serie TV altamente valutate[/COLOR][/B]", tvshow_highly_rated_image),
+	("[B][COLOR lime]Serie TV divise per anno[/COLOR][/B]", tvshow_years_image),
 	("[B][COLOR lime]Serie TV per genere[/COLOR][/B]", tvshow_genres_image)
 ]
 
 messages = {
 	"start_search": {
-		"title": "[B][COLOR lime]In Lavorazione[/COLOR][/B]",
+		"title": "[B][COLOR cyan]Attendere prego    by Area69[/COLOR][/B]",
 		"text": "[B][COLOR lime]Inizio ricerca...[/COLOR][/B]"
 	},
 
 	"movie": {
-		"popular": "[B][COLOR lime]Ricerca film famosi in corso...[/COLOR][/B]",
+		"popular": "[B][COLOR lime]Ricerca film più popolari in corso...[/COLOR][/B]",
 		"top_rated": "[B][COLOR lime]Ricerca film più votati in corso...[/COLOR][/B]",
-		"discover": "[B][COLOR lime]Sto scoprendo nuovi film...",
-		"genre": "[B][COLOR lime]Ricerca film per genere in corso...[/COLOR][/B]",
-		"year": "[B][COLOR lime]Ricerca film per anno in corso...[/COLOR][/B]",
-		"default": "[B][COLOR lime]Sto cercando film con questo titolo[/COLOR][/B]"
+		"discover": "[B][COLOR lime]Ricerco i film più valutati in corso...[/COLOR][/B]",
+		"genre": "[B][COLOR lime]Ricerca film per genere scelto in corso...[/COLOR][/B]",
+		"year": "[B][COLOR lime]Ricerca film per anno scelto in corso...[/COLOR][/B]",
+		"default": "[B][COLOR lime]Ricerco film con il titolo scelto in corso...[/COLOR][/B]"
+		
 	},
 
 	"tvshow": {
-		"popular": "[B][COLOR lime]Ricerca serie tv famosi in corso...[/COLOR][/B]",
-		"top_rated": "[B][COLOR lime]Ricerca serie tv più votati in corso...[/COLOR][/B]",
-		"discover": "[B][COLOR lime]Sto scoprendo nuove serie tv...[/COLOR][/B]",
-		"genre": "[B][COLOR lime]Ricerca serie tv per genere in corso...[/COLOR][/B]",
-		"year": "[B][COLOR lime]Ricerca serie tv per anno in corso...[/COLOR][/B]",
-		"default": "[B][COLOR lime]Sto cercando serie tv con questo titolo[/COLOR][/B]"
+		"popular": "[B][COLOR lime]Ricerca serie tv più popolari in corso...[/COLOR][/B]",
+		"top_rated": "[B][COLOR lime]Ricerca serie tv più votata in corso...[/COLOR][/B]",
+		"discover": "[B][COLOR lime]Ricerco le serie tv scelta in corso...[/COLOR][/B]",
+		"genre": "[B][COLOR lime]Ricerca serie tv per genere scelto in corso...[/COLOR][/B]",
+		"year": "[B][COLOR lime]Ricerca serie tv per anno scelto in corso...[/COLOR][/B]",
+		"default": "[B][COLOR lime]Riceco la serie tv con il titolo scelto in corso...[/COLOR][/B]"
 	},
 
 	"stream": {
-		"error_stream_title": "[B][COLOR red]ERRORE TITOLO STREAM[/COLOR][/B]",
-		"error_stream_text": "[B][COLOR red]ERRORE TESTO STREAM[/COLOR][/B]"
+		"error_stream_title": "[B][COLOR cyan]A T T E N Z I O N E[/COLOR][/B]",
+		"error_stream_text": "[B][COLOR lime]Riproduzione fallita di uno o piu elementi, prova altra sorgente.[/COLOR][/B]"
 	},
 
-	"person": "[B][COLOR lime]Persone[/COLOR][/B]",
+	"person": "[B][COLOR lime]Ricerco Attore scelto in corso...[/COLOR][/B]",
 	"movie_list": "[B][COLOR lime]Lista Film[/COLOR][/B]",
 	"tvshow_list": "[B][COLOR lime]Lista Serie Tv[/COLOR][/B]",
-	"episode": "[B][COLOR lime]episodi[/COLOR][/B]",
-	"playing": "[B][COLOR lime]buona visione[/COLOR][/B]"
+	"episode": "[B][COLOR lime]Episodi[/COLOR][/B]",
+	"playing": "[B][COLOR lime]Buona Visione[/COLOR][/B]"
 }
